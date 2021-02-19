57b782029a9c3893b01389e93f6352e9897d36a5
PAVAN-BORADE <76805338+PAVAN-BORADE@users.noreply.github.com>
2021-02-19 10:34:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var images = ["49611016.jpg","Shivkumar.jpg", "Gauri.jpg" , "Pavan.jpg","Granny.jpg","Grand father.jpg","Bhagyashari.jpg"];
+ var names = ["Family Book","Shivkumar borade", "Gauri borade", "Pavan borade","Jaymala Borade","Anil Borade","Bhagyashri Borade"]; 
+ var i = 0; function update() { i++; 
+    var numbers_of_family_member_in_array = 6
+    if(i > numbers_of_family_member_in_array ) { i = 0; } 
+    var updatedImage = images[i]; 
+    var updatedName = names[i]; 
+    document.getElementById("family_member_image").src = updatedImage; 
+    document.getElementById("family_member_name").innerHTML = updatedName; }
